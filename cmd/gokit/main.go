85d05fd389229cc4fb296a98cb53b40342bada84
Hassan Agyemang Boakye <128396960+codedbyhassan@users.noreply.github.com>
2026-09-02 11:13:14 +0000

    feat(frontend): expose Go web UI and fix CLI input joining

--- cmd/gokit/main.go
+++ cmd/gokit/main.go
@@ -5,15 +5,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	gokithttp "github.com/codedbyhassan/gokit/api/http"
 	"github.com/codedbyhassan/gokit/interpret/pipeline"
+	"github.com/codedbyhassan/gokit/web/frontend"
 )
 
 func main() {
 	serve := flag.Bool("serve", false, "start the GoKit HTTP API")
+	web := flag.Bool("web", false, "start the GoKit web frontend")
 	addr := flag.String("addr", ":8080", "HTTP listen address")
 	flag.Parse()
+
+	if *web {
+		server, err := frontend.NewServer()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("GoKit web frontend listening on %s", *addr)
+		log.Fatal(http.ListenAndServe(*addr, server.Handler()))
+	}
 
 	if *serve {
 		log.Printf("GoKit API listening on %s", *addr)
@@ -21,8 +33,13 @@ func main() {
 	}
 
 	input := flag.Args()
-	if len(input) == 0 { fmt.Println("usage: gokit [--serve] [--addr :8080] <input>"); return }
-	result, err := pipeline.Parse(fmt.Sprint(input...))
-	if err != nil { log.Fatal(err) }
+	if len(input) == 0 {
+		fmt.Println("usage: gokit [--serve|--web] [--addr :8080] <input>")
+		return
+	}
+	result, err := pipeline.Parse(strings.Join(input, " "))
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(result.Value)
 }
